7a538e20c62664917b52e214a2aa7aa10dd5e553
ymzk-jp <masaodx007@gmail.com>
2018-06-20 03:30:47 +0900

    README.md

--- answer/example/47/fadeImage.js
+++
@@ -1,9 +0,0 @@
-$('.fade').on('click', function () {
-  if ($('.fade').hasClass('fade_image')) {
-    $('.fade').removeClass('fade_image');
-    $('img').remove();
-  } else {
-    $('.fade').addClass('fade_image');
-    $('.fade').append('<img src=\"../img/somari.jpg\" alt=\"somari\">');
-  }
-});
