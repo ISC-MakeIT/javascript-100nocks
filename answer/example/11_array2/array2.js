93f00efedda16351014e44cd76e32eace1b4d6f4
ymzk-jp <masaodx007@gmail.com>
2018-06-26 14:52:59 +0900

    9,11,27,28

--- answer/example/11_array2/array2.js
+++ answer/example/11_array2/array2.js
@@ -3,3 +3,7 @@ const ary = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
 ary.forEach(function (v, index) {
     console.log(v + index);
 });
+// both
+// for (let i in ary) {
+//     console.log(ary[i] + i);
+// }
