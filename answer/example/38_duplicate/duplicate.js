2331f913e71be71a6a729b38daefc9d9709f1c5d
ymzk-jp <masaodx007@gmail.com>
2018-06-28 00:57:33 +0900

    mod 38

--- answer/example/38_duplicate/duplicate.js
+++ answer/example/38_duplicate/duplicate.js
@@ -1,29 +1,4 @@
 'use strict';
-<<<<<<< HEAD
-let res = randomization(10, 10);
-console.log(res);
-console.log('length ' + res.length);
-
-/**
- * 
- * @param {number} has_order 
- * @param {number} max 
- * @returns {array} this one has has_order in 1 ~ max
- */
-function randomization(has_order, max) {
-    // if (!has_order <= max) {
-    //     return 0;
-    // }
-    let data = [];
-    let num = 0;
-    while (data.length < has_order) {
-        num = ~~(Math.random() * max);
-        if (!isExistArray(data, num)) {
-            data.push(num);
-        }
-    }
-    return data;
-=======
 randomization(25, 75);
 
 /*
@@ -41,52 +16,15 @@ function randomization(has_order, max) {
         }
     }
     console.log(data);
->>>>>>> master
 }
 
-/**
- * 
-<<<<<<< HEAD
- * @param {Array} ar 
- * @param {number} x 
- * @returns {boolean} existed => true
- */
-function isExistArray(ar, x) {
-    if (x === 0) {
-        return 'ture';
-    }
-=======
+/** 
  * @param {number} x 
  * @param {Array} ar 
  */
 function isExistArray(x, ar) {
->>>>>>> master
     let res = ar.some(function (v) {
         return v === x;
     });
     return res;
-<<<<<<< HEAD
 }
-
-
-// function randomization_old(len, max) {
-//     if (len <= max) {
-//         let d = [];
-//         let num = 0;
-//         let res = 'false';
-//         while (d.length < len) {
-//             num = Math.floor(Math.random() * max);
-//             res = isExistArray(d, num);
-//             if (!res) {
-//                 d.push(num);
-//             }
-//         }
-//         // let e = d.sort(function (a, b) {
-//         //     return (a < b ? -1 : 1);
-//         // });
-//         return d;
-//     }
-// }
-=======
-}
->>>>>>> master
