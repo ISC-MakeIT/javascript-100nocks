923c84e9c251120ca52734a94d337c90e8d7d368
itti1021 <itti.noritokann@gmail.com>
2018-06-29 19:02:54 +0900

    課題14修正

--- answer/itti1021/14_concat/concat.js
+++ answer/itti1021/14_concat/concat.js
@@ -1,13 +1,13 @@
-var ary1 = ["'a'", "'b'"];
-var ary2 = ["'c'", "'d'"];
+var ary1 = ["a", "b"];
+var ary2 = ["c", "d"];
 
 var array = ary1.concat(ary2);
 
 function output() {
-    document.getElementById("name").innerHTML = "["
-    document.getElementById("name").innerHTML += " " + array[0] + " ";
+    var str = `[ \'${array[0]}\'`;
     for (var i = 1; i < 4; i++) {
-        document.getElementById("name").innerHTML += "," + " " + array[i] + " ";
-    };
-    document.getElementById("name").innerHTML += "]";
+        str += ` , \'${array[i]}\'`
+    }
+    str += ` ]`
+    document.getElementById("name").innerHTML = str;
 }
