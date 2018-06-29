79795443c846d04b3aeea58bbcff21236febe68d
itti1021 <itti.noritokann@gmail.com>
2018-06-29 12:37:54 +0900

    微修正

--- answer/itti1021/14_concat/concat.js
+++ answer/itti1021/14_concat/concat.js
@@ -3,14 +3,6 @@ var ary2 = ["'c'", "'d'"];
 
 var array = ary1.concat(ary2);
 
-// function output() {
-//     var name = document.getElementById("name");
-//     name.insertAdjacentHTML("afterbegin", "[");
-//     array.forEach(function (n) {
-//         name.insertAdjacentHTM("beforeend", "n")
-//     });
-// }
-
 function output() {
     document.getElementById("name").innerHTML = "["
     document.getElementById("name").innerHTML += " " + array[0] + " ";
