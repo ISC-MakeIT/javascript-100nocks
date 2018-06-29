fe8e9e737f20076555de73fcb536908789045756
itti1021 <itti.noritokann@gmail.com>
2018-06-29 10:49:50 +0900

    課題13

---
+++ answer/itti1021/13_typeof/typeof.js
@@ -0,0 +1,11 @@
+var num = 1;
+var zero = 0;
+var str = "1";
+var zs = "0";
+
+function judging() {
+    document.getElementById("num").innerHTML = typeof(num);
+    document.getElementById("zero").innerHTML = typeof(zero);
+    document.getElementById("str").innerHTML = typeof(str);
+    document.getElementById("zs").innerHTML = typeof(zs);
+}
