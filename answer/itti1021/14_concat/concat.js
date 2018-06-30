f5189089294c719de223862be64138edfc026dc6
itti1021 <itti.noritokann@gmail.com>
2018-06-30 18:27:08 +0900

    課題15と微修正

--- answer/itti1021/14_concat/concat.js
+++ answer/itti1021/14_concat/concat.js
@@ -4,9 +4,9 @@ var ary2 = ["c", "d"];
 var array = ary1.concat(ary2);
 
 function output() {
-    var str = `[ \'${array[0]}\'`;
+    var str = `[ '${array[0]}'`;
     for (var i = 1; i < 4; i++) {
-        str += ` , \'${array[i]}\'`;
+        str += ` , '${array[i]}'`;
     }
     str += ` ]`;
     document.getElementById("name").innerHTML = str;
