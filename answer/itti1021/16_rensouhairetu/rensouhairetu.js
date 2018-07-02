20126c8857e3ebffe8fe701a542f6ac1a6595b68
itti1021 <itti.noritokann@gmail.com>
2018-07-02 13:53:05 +0900

    修正

--- answer/itti1021/16_rensouhairetu/rensouhairetu.js
+++ answer/itti1021/16_rensouhairetu/rensouhairetu.js
@@ -1,9 +1,12 @@
 var ary = {"color" : "red", "size" : "100"};
 ary["count"] = "5";
+var message = "";
 
 function output() {
     for (var key in ary) {
         var value = ary[key];
-        document.getElementById("result").innerHTML += `keyは: ${key} valueは: ${value} <br>`;
+        message += `keyは: ${key} valueは: ${value} <br>`;
+        // message += `keyは: ${key} valueは: ${value} \n`;
     }
+    document.getElementById("result").innerHTML = message;
 }
