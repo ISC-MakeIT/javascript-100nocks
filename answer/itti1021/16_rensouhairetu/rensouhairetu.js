f45382efed6118235884b36a8d1e6a1d047eec9c
itti1021 <itti.noritokann@gmail.com>
2018-07-02 14:20:07 +0900

    微修正

--- answer/itti1021/16_rensouhairetu/rensouhairetu.js
+++ answer/itti1021/16_rensouhairetu/rensouhairetu.js
@@ -1,8 +1,8 @@
 var ary = {"color" : "red", "size" : "100"};
 ary["count"] = "5";
-var message = "";
 
 function output() {
+    var message = "";
     for (var key in ary) {
         var value = ary[key];
         message += `keyは: ${key} valueは: ${value} <br>`;
