1f0afd1011ab8c44a0f18766308ac2f27055cc3e
itti1021 <itti.noritokann@gmail.com>
2018-07-03 16:53:46 +0900

    課題22と修正

--- answer/itti1021/20_encrypto1/encrypto1.js
+++ answer/itti1021/20_encrypto1/encrypto1.js
@@ -1,14 +1,10 @@
 var raw = "114514";
 var str = "";
-const ary = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 for (var i = 0; i < raw.length; i++) {
-    str += `${raw.charAt(i)}`;
-    str += `${ary[Math.floor(Math.random() * ary.length)]}`;
-    str += `${ary[Math.floor(Math.random() * ary.length)]}`;
+    str += raw.charAt(i);
+    str += Math.floor(Math.random() * 10);
+    str += Math.floor(Math.random() * 10);
 }
-
-// raw = str;
-// console.log(raw);
 
 console.log(str);
--- answer/itti1021/21_decrypto1/decrypto.js
+++ answer/itti1021/21_decrypto1/decrypto.js
@@ -3,9 +3,8 @@ var str = "";
 
 for (var i = 0; i < crypto.length; i++) {
     if ((i % 3 ) != 2) {
-        str += `${crypto.charAt(i)}`;
+        str += crypto.charAt(i);
     }
-    else {}
 }
 
 console.log(str);
