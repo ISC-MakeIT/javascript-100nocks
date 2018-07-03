2c02129e974c5c74633d843850f353ca9a4d6832
itti1021 <itti.noritokann@gmail.com>
2018-07-03 11:39:17 +0900

    課題21

---
+++ answer/itti1021/21_decrypto1/decrypto.js
@@ -0,0 +1,11 @@
+const crypto = "やくこみがんきいばていんるよは";
+var str = "";
+
+for (var i = 0; i < crypto.length; i++) {
+    if ((i % 3 ) != 2) {
+        str += `${crypto.charAt(i)}`;
+    }
+    else {}
+}
+
+console.log(str);
