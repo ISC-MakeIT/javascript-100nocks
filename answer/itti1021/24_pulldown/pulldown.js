486f89f65a5cba5339003c74e641698193666c85
itti1021 <itti.noritokann@gmail.com>
2018-07-04 10:44:06 +0900

    課題24

---
+++ answer/itti1021/24_pulldown/pulldown.js
@@ -0,0 +1,9 @@
+function pulldown() {
+    let select = document.getElementById("hoge");
+    for (let i = 1; i <= 200; i++) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.innerText = i;
+        select.appendChild(option);
+    }
+}
