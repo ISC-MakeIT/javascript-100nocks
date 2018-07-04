397f94dce3cc766642645b1c4dd3121639a41259
itti1021 <itti.noritokann@gmail.com>
2018-07-04 11:50:45 +0900

    微修正

--- answer/itti1021/24_pulldown/pulldown.js
+++ answer/itti1021/24_pulldown/pulldown.js
@@ -1,6 +1,6 @@
 function pulldown() {
     let select = document.getElementById("hoge");
-    for (let i = 1; i <= 200; i++) {
+    for (let i = 0; i <= 200; i++) {
         let option = document.createElement("option");
         option.value = i;
         option.innerText = i;
