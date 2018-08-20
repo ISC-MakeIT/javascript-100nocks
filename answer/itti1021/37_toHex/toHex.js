3b4301e7da6d390e0be07a1606858e2d2e066a2a
itti1021 <itti.noritokann@gmail.com>
2018-08-20 23:35:12 +0900

    name名おかしかったので修正

--- answer/itti1021/37_toHex/toHex.js
+++ answer/itti1021/37_toHex/toHex.js
@@ -1,4 +1,4 @@
 function change() {
-    const num = parseInt(document.to_binary_form.elements[0].value);
+    const num = parseInt(document.to_hexadecimal_form.elements[0].value);
     document.getElementById("result").innerHTML = num.toString(16);
 }
